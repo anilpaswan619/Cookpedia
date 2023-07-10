579470876548e0e85d0d55600d9fe004277c5eb9
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-07-10 18:44:21 +0530

    Updated

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -11,7 +11,7 @@ export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [ 
-    new Recipe('Veggy Soup', 'By Jeniffer Lay', '/assets/images/salad.jpg '), 
+    new Recipe('Vegan Soup', 'By Jeniffer Lay', '/assets/images/salad.jpg '), 
     new Recipe('Juicy Noodles', 'By Smith John', '/assets/images/noodles.jpg'),
     new Recipe('Creamy Pancake', 'By Luise De', '/assets/images/creamy.jpg'),
     new Recipe('Fried Beans', 'By Garry Don', '/assets/images/beans.jpg'),
