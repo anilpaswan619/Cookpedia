c765647eb77a231dd9391f82147da7bc74abf004
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-08-11 19:23:18 +0530

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component'; 
+import { CarouselComponent} from './carousel/carousel.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'carousel', component: CarouselComponent },
 
 ];
 
